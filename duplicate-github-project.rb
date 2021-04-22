93de5c22570830ef6b35e6efd17e3a4ed4e191ef
Yutaka <y.tajika529@gmail.com>
2021-04-23 06:07:36 +0900

    modified: Formula file

--- duplicate-github-project.rb
+++ duplicate-github-project.rb
@@ -1,16 +1,16 @@
 class DuplicateGithubProject < Formula
-  desc "This command duplicates a GitHub repository project, including its cards."
-  homepage ""
+  desc "This command duplicates a GitHub repository project, including its cards"
+  homepage "https://github.com/taji-taji/DuplicateGithubRepositoryProject"
   url "https://github.com/taji-taji/DuplicateGithubRepositoryProject/archive/refs/tags/0.1.0.tar.gz"
   sha256 "bc36efb44dc673963a085304d2b04504c3eda20ee99caff16e4eadd36e708ec2"
   license ""
 
-  def install
-    system "make", "install", "PREFIX=#{prefix}"
-  end
-
   bottle do
     root_url "https://github.com/taji-taji/DuplicateGithubRepositoryProject/releases/download/0.1.0"
     sha256 cellar: :any_skip_relocation, big_sur: "4fdc7ac28bda9156bf557e359d27d3615cce05722c4ffa695e7d9b66c56a29ba"
+  end
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
   end
 end
